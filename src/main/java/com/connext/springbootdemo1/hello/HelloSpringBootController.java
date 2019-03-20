0ddd33ac66a3886dacae6f623d315c6dc7e77af7
cctchangtong <809577371@qq.com>
2019-03-20 10:02:09 +0800

    rename file

--- src/main/java/com/connext/springbootdemo1/hello/HelloSpringBootController.java
+++ src/main/java/com/connext/springbootdemo1/hello/HelloSpringBootController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 19-3-19
  */
 @RestController
-public class HelloSpringBoot {
+public class HelloSpringBootController {
     @Autowired
     private StudentDao studentDao;
 //    @RequestMapping(path = {"/helloSpringBoot"})
